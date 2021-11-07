0444f848650957d11186c66b1fe1f92cb121d319
Luke Batema <lukasbatema@gmail.com>
2021-11-07 16:50:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "pyslots=PYSlots.__main__:main",
+            "pyslots=pyslots.__main__:main",
         ]
     },
 )
