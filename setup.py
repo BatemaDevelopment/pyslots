c581ca8d43b743c535ed644180662adb5d18e7d6
Lukas-Batema <LukasBatema@gmail.com>
2021-11-08 10:31:20 +0000

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="PYSlots",
-    version="1.0.698",
+    version="1.0.6991",
     author="Lukas-Batema",
     author_email="lukasbatema@gmail.com",
     description="A fun terminal Python slots game!",
