94c4d38362febedf89561a5cc4e710fd39333935
Luke Batema <lukasbatema@gmail.com>
2021-11-07 19:33:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from os.path import dirname, join
 import setuptools
-import py_slots.__init__ as __init__
+import PYSlots.__init__ as __init__
 
 setuptools.setup(
     name="PYSlots",
@@ -20,13 +20,13 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["py_slots"],
+    packages=["PYSlots"],
     install_requires=["wheel", "setuptools"],
     keywords=["slots", "python", "py"],
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "pyslots=py_slots.__main__:main",
+            "pyslots=PYSlots.__main__:main",
         ]
     },
 )
